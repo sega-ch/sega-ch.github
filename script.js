efb7affad17e23bd4cf809db722d6b1a8b34abc8
sega-ch <59026328+sega-ch@users.noreply.github.com>
2022-10-29 00:41:55 +0500

    js file

---
+++ script.js
@@ -0,0 +1,14 @@
+function checkPwd()
+{
+    var password = document.getElementById("psw");
+    var userPwd = password;
+
+    if (userPwd==="7496Sp")
+    {
+        
+        return true;
+
+    }
+    alert("Иди НАхуй Чёрт Сука! <3");
+    return false;
+}
